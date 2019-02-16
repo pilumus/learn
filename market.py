fc864e673b094280a3ffbb104b30f73f2543cdb2
pilumus <vlad-sid96@yandex.ru>
2019-02-16 13:38:26 +0300

    add market file

---
+++ market.py
@@ -0,0 +1,14 @@
+class Enterprise(c, s, m = 100, p):
+    self.capital = c
+    self.store = s
+    self.money = m
+    self.production = p
+
+
+class Worker(t = 8, p, m = 10, c):
+    self.production = p
+    self.money = m
+    self.time = t
+    self.consume = c
+
+    def get_job
